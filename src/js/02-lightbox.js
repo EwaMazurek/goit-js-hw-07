afc42e31ea683d8c26eb14dc2e69ab5b6bcfd593
EwaMazurek <ewa.mmazurek@gmail.com>
2023-01-23 12:05:55 +0100

    task 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach(img => {
+  gallery.insertAdjacentHTML(
+    "beforeend",
+    `<a class="gallery__item" href=${img.original}>
+<img class="gallery__image" src=${img.preview} alt=${img.description} />
+</a>`
+  );
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
